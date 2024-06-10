19655a5200cf5f85be90b7dfe4bdd65235e1682b
Donat <tasidonat@gmail.com>
2024-06-10 21:00:10 +0200

    Home Page

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use App\Models\Post;
+use Illuminate\Http\Request;
+
+class HomeController extends Controller
+{
+    /**
+     * Handle the incoming request.
+     */
+    public function __invoke(Request $request)
+    {
+        return view('home', [
+            'featuredPosts' => Post::published()->featured()->latest('published_at')->take(3)->get(),
+            'latestPosts' => Post::published()->latest('published_at')->take(9)->get(),
+        ]);
+    }
+}
