440b10ee5854fec3709a3a80f0424a3a7f8987ec
Donat <tasidonat@gmail.com>
2024-06-25 21:09:42 +0200

    Eager Loading on database queries

--- resources/views/components/posts/post-card.blade.php
+++ resources/views/components/posts/post-card.blade.php
@@ -7,8 +7,8 @@
     </a>
     <div class="mt-3">
         <div class="flex items-center mb-2 gap-x-2">
-            @if ($post->categories()->first())
-                <x-posts.category-badge :category="$post->categories()->first()" />
+            @if ($category = $post->categories->first())
+                <x-posts.category-badge :category="$category" />
             @endif
             <p class="text-gray-500 text-sm">{{ $post->published_at }}</p>
         </div>
